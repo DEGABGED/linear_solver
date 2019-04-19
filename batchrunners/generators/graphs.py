b2ba53ab4af5685b28937c4062af0c57174add6c
Francis dela Cruz <franciszac.dlc@gmail.com>
2019-04-20 00:45:12 +0800

    fasfasdfas asymm demand graphs

--- batchrunners/generators/graphs.py
+++ batchrunners/generators/graphs.py
@@ -13,6 +13,7 @@ from ctmmodels.delaythroughput import DelayThroughputSimplex as Model
 class GraphGenerator(object):
 
     def __init__(self, headless=True, time_range=TIME_RANGE, simplex_range=10, folder=None, image_path=IMAGE_PATH):
+        self.headless = headless
         self.time_range = time_range
         self.time_ticks = np.arange(0, time_range+1, 1)
         self.simplex_range = simplex_range
@@ -111,15 +112,19 @@ class GraphGenerator(object):
             thru_dict[(row[-3]*self.simplex_range, row[-2]*self.simplex_range, row[-1]*self.simplex_range)] = row[3]
             obj_dict[(row[-3]*self.simplex_range, row[-2]*self.simplex_range, row[-1]*self.simplex_range)] = row[4]
 
-        self.plot_simplex(delay_dict, "{} (Delay)".format(title_partial), "{}_delay.png".format(filename_partial))
-        self.plot_simplex(thru_dict, "{} (Throughput)".format(title_partial), "{}_throughput.png".format(filename_partial))
-        self.plot_simplex(obj_dict, "{} (Objective Value)".format(title_partial), "{}_obj.png".format(filename_partial))
-        self.plot_simplex(runtime_dict, "{} (Runtime)".format(title_partial), "{}_runtime.png".format(filename_partial))
+        self.plot_simplex(delay_dict, "{} (Delay)".format(title_partial), "{}_delay.png".format(filename_partial), cb_kwargs={'vmin': 1200, 'vmax': 6000})
+        self.plot_simplex(thru_dict, "{} (Throughput)".format(title_partial), "{}_throughput.png".format(filename_partial), cb_kwargs={'vmin': 42, 'vmax': 45})
+        self.plot_simplex(obj_dict, "{} (Objective Value)".format(title_partial), "{}_obj.png".format(filename_partial), cb_kwargs={'vmin': -3000, 'vmax': 5000})
+        self.plot_simplex(runtime_dict, "{} (Runtime)".format(title_partial), "{}_runtime.png".format(filename_partial), cb_kwargs={'vmin': 1, 'vmax': 100})
 
-    def plot_simplex(self, data, title, filename):
-        fig, tax = ternary.figure(scale=self.simplex_range)
-        fig.set_size_inches(10, 8)
-        tax.heatmap(data, style="triangular")
+    def plot_simplex(self, data, title, filename, tax=None, cb_kwargs={}):
+        if tax is None:
+            fig, tax = ternary.figure(scale=self.simplex_range)
+            fig.set_size_inches(10, 8)
+        else:
+            fig = None
+
+        tax.heatmap(data, style="triangular", **cb_kwargs)
         tax.boundary(linewidth=2.0)
 
         tax.set_title(title)
@@ -129,9 +134,11 @@ class GraphGenerator(object):
 
         tax.ticks(axis='lbr', linewidth=1, multiple=5)
         tax.clear_matplotlib_ticks()
+        tax._redraw_labels()
 
-        if self.headless:
-            fig.savefig(os.path.join(self.image_path, self.folder, filename))
-            plt.close(fig)
-        else:
-            return fig
+        if fig is not None:
+            if self.headless:
+                fig.savefig(os.path.join(self.image_path, self.folder, filename))
+                plt.close(fig)
+            else:
+                return fig
